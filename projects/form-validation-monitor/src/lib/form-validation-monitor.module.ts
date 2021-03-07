706a80860662db9a089592d1653f9dc5cb2cb96a
LK <laszlo.kovary@gmail.com>
2021-03-07 13:52:42 +0100

    removed BrowserModule from the lib

--- projects/form-validation-monitor/src/lib/form-validation-monitor.module.ts
+++ projects/form-validation-monitor/src/lib/form-validation-monitor.module.ts
@@ -1,7 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
 import { FormValidationMonitorComponent } from './form-validation-monitor.component';
 
 
@@ -9,7 +8,6 @@ import { FormValidationMonitorComponent } from './form-validation-monitor.compon
 @NgModule({
   declarations: [FormValidationMonitorComponent],
   imports: [
-    BrowserModule,
     FormsModule,    
     CommonModule
   ],
